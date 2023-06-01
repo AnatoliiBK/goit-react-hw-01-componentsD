9fd41bfc6efe3d315d43de499baee2e8a062deb7
AnetRo <beatrice.ccq@gmail.com>
2023-06-01 22:36:46 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,7 @@ import { FriendList } from './friends/FriendList.jsx';
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      
     >
       React homework template
       <FriendList friends={friends} />
