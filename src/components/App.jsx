10fde5141d4e0a8f81ef1380abc1790577fa2906
AnetRo <beatrice.ccq@gmail.com>
2023-06-01 23:09:20 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
+import user from "./user/user.json";
+import { Profile } from "./user/Profile.jsx";
 export const App = () => {
   return (
     <div>
-      
+      <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+       />
     </div>
   );
 };
