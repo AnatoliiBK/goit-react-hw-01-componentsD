38d4948ed55e6248fc0d9a939992543dc3e6f747
AnetRo <beatrice.ccq@gmail.com>
2023-06-01 23:20:35 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,5 @@
 import user from "./user/user.json";
-import data from "./statistic/statistics.json";
-import friends from "./friends/friends.json";
-import transactions from "./transactions/transactions.json";
 import { Profile } from "./user/Profile.jsx";
-import { Statistics } from './statistic/Staistics.jsx';
-import { FriendList } from './friends/FriendList.jsx';
-import { TransactionHistory } from './transactions/Transactions.jsx';
 
 export const App = () => {
   return (
@@ -17,9 +11,6 @@ export const App = () => {
       avatar={user.avatar}
       stats={user.stats}
        />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
     </div>
   );
 };
